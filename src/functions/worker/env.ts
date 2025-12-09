7890bd6c5b8f1bf051481189f1e24a73d8cce7ca
Claudan <claudioandradecor@gmail.com>
2025-12-09 09:28:05 -0300

    feat: add MELI_AUTH_SECRET_ARN to worker environment configuration

--- src/functions/worker/env.ts
+++ src/functions/worker/env.ts
@@ -2,6 +2,7 @@ import { requireEnv } from "../../../shared/env-utils.js";
 
 export const WORKER_ENV = {
   DYNAMO_TABLE: requireEnv("DYNAMO_TABLE"),
+  MELI_AUTH_SECRET_ARN: requireEnv("MELI_AUTH_SECRET_ARN"),
   REGION: requireEnv("AWS_REGION"),
   TELEGRAM_SECRET_ARN: requireEnv("TELEGRAM_SECRET_ARN"),
 };
